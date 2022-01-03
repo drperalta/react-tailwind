13e17226936682d801d6e53681da6ef68abca171
David Peralta <peraltadavidr@outlook.com>
2022-01-03 10:53:35 +0800

    intial commit + eslint

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="font-bold text-gray-800 text-lg">Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
